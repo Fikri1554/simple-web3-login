745f90df12aa85496d6045ffb58c5d7f60f3bd8a
Muhammad Fikri <66526582+Fikri1554@users.noreply.github.com>
2022-04-11 02:58:10 +0700

    Simple Web3 Login

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import logo from "./MageLogo.jpg";
+import React, { useEffect } from "react";
+import "./App.css";
+import { useMoralis } from "react-moralis";
+import { Button, Box, Heading } from "@chakra-ui/react";
+import { Container, Center } from "@chakra-ui/react";
+
+const LogoutButton = () => {
+  const { logout, isAuthenticating } = useMoralis();
+
+  return (
+    <Button
+      display={"block"}
+      colorScheme="red"
+      variant="solid"
+      isLoading={isAuthenticating}
+      onClick={() => logout()}
+      disabled={isAuthenticating}>
+      Logout
+    </Button>
+  );
+};
+
+// ---------- APP -------------
+function App() {
+  const {
+    authenticate,
+    isWeb3Enabled,
+    isAuthenticated,
+    user,
+    enableWeb3,
+    Moralis,
+  } = useMoralis();
+
+  async function authWalletConnect() {
+    const user = authenticate({
+      provider: "walletconnect",
+      chainId: 56,
+      mobileLinks: [
+        "metamask",
+        "trust",
+        "rainbow",
+        "argent",
+        "imtoken",
+        "pillar",
+      ],
+      signingMessage: "Welcome!",
+    });
+    console.log(user);
+  }
+
+  useEffect(() => {
+    if (!isWeb3Enabled && isAuthenticated) {
+      enableWeb3({ provider: "walletconnect", chainId: 56 });
+      console.log("web3 activated");
+    }
+  }, [isWeb3Enabled, isAuthenticated, enableWeb3]);
+
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+      window.localStorage.removeItem("WALLETCONNECT_DEEPLINK_CHOICE");
+    }
+  });
+
+  // ----- Authenticate in Metamask---------
+  if (!isAuthenticated && !user) {
+    console.log(user);
+    return (
+      <Container maxW="container.lg">
+        <br />
+        <Center>
+          <Heading as="h2" size="2xl" p={10}>
+            Connect Wallet
+          </Heading>
+        </Center>
+        <Center>
+          <Button
+            colorScheme="green"
+            size="lg"
+            onClick={() => authenticate({ signingMessage: "Hello" })}>
+            Sign in using Metamask
+          </Button>
+        </Center>
+        <br />
+        <Center>
+          <Button
+            colorScheme="green"
+            size="lg"
+            onClick={() => authWalletConnect()}>
+            Sign in using Wallet Connect
+          </Button>
+        </Center>
+        <br />
+      </Container>
+    );
+  }
+
+  return (
+    <Box display={"flex-start"} p={35} className="App">
+      <LogoutButton />
+      <Center>
+        <Heading as="h2" size="1xl" p={10}>
+          Wallet Logged in
+        </Heading>
+      </Center>
+    </Box>
+  );
+}
+
+export default App;
